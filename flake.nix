73242d378cd65e4f5ee3dc1cb37efa38baad29ea
Harry Maclean <harry@hmac.dev>
2024-02-15 16:59:15 +0000

    fix bug

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Reproduction of a bug";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -10,13 +10,14 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        toml = (pkgs.formats.toml { }).generate "foo.toml" { };
+        toml = (pkgs.formats.toml { }).generate "foo.toml" { hello = "world"; };
       in
       {
         packages.default = pkgs.stdenv.mkDerivation {
           name = "toml-bug";
           src = ./.;
           installPhase = ''
+            mkdir $out
             cp ${toml} $out/foo.toml
           '';
         };
